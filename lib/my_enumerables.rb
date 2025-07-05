576baef20b8abafbb28aeef9acc5ae55fea32bfd
objones25 <59661118+objones25@users.noreply.github.com>
2025-07-05 16:11:28 -0700

    all tests passing

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,89 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index(&block)
+    return to_enum(:my_each_with_index) unless block_given?
+
+    index = 0
+    my_each do |element|
+      yield(element, index)
+      index += 1
+    end
+    self
+  end
+
+  def my_select(&block)
+    return to_enum(:my_select) unless block_given?
+
+    result = []
+    my_each do |element|
+      result << element if yield(element)
+    end
+    result
+  end
+
+  def my_all?(&block)
+    return to_enum(:my_all?) unless block_given?
+
+    my_each do |element|
+      return false unless yield(element)
+    end
+    true
+  end
+
+  def my_any?(&block)
+    return to_enum(:my_any?) unless block_given?
+
+    my_each do |element|
+      return true if yield(element)
+    end
+    false
+  end
+
+  def my_map(&block)
+    return to_enum(:my_map) unless block_given?
+
+    result = []
+    my_each do |element|
+      result << yield(element)
+    end
+    result
+  end
+
+  def my_none?(&block)
+    return to_enum(:my_none?) unless block_given?
+
+    my_each do |element|
+      return false if yield(element)
+    end
+    true
+  end
+
+  def my_count(&block)
+    return self.length unless block_given?
+
+    count = 0
+    my_each do |element|
+      count += 1 if yield(element)
+    end
+    count
+  end
+
+  def my_inject(initial_value = nil, &block)
+    return to_enum(:my_inject) unless block_given?
+  
+    if initial_value
+      result = initial_value
+      start_index = 0
+    else
+      result = self.first
+      start_index = 1
+    end
+
+    my_each_with_index do |element, index|
+      result = yield(result, element) if index >= start_index
+    end
+    result
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +92,12 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each(&block)
+    return to_enum(:my_each) unless block_given?
+
+    for i in 0...self.length
+      yield(self[i])
+    end
+    self
+  end
 end
